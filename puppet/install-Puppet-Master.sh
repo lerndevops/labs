d99f5be183d7cb4deba3bede0cd713c7eb92ae95
lerndevops <45846027+lerndevops@users.noreply.github.com>
2019-08-06 12:13:38 +0530

    Update install-Puppet-Master.sh

--- puppet/install-Puppet-Master.sh
+++ puppet/install-Puppet-Master.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+cd /tmp ; sudo curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+cd /tmp ; sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+sudo apt-get install -y puppetserver
+
+sudo systemctl start puppetserver
+sudo systemctl status puppetserver
+# sudo systemctl enable puppetserver  # for enabling boot time start time

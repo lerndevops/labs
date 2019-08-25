a4553d576946333a75729d32639bc2fd1c333793
root <root@ip-172-31-5-52.us-east-2.compute.internal>
2019-08-25 06:24:46 +0000

    updated installDocker.sh

--- docker/installDocker.sh
+++ docker/installDocker.sh
@@ -21,7 +21,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 ## Install Docker latest
 
 sudo apt-get update ; clear
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo service docker start
 
 ## curl -fsSL https://get.docker.com -o get-docker.sh

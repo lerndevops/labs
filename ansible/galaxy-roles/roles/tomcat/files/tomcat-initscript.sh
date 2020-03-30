688dc31b0561940cdb72d163eca9de8f042c1a18
lerndevops <45846027+lerndevops@users.noreply.github.com>
2020-03-30 19:21:19 +0530

    Update tomcat-initscript.sh

--- ansible/galaxy-roles/roles/tomcat/files/tomcat-initscript.sh
+++ ansible/galaxy-roles/roles/tomcat/files/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
  
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/lib/jvm/jre
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
  
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH

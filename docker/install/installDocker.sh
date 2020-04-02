66ea275d980295465a12b144b805820c7a2bdd48
lerndevops <lerndevops@gmail.com>
2020-04-02 07:07:24 -0700

    docker

--- docker/install/installDocker.sh
+++ docker/install/installDocker.sh
@@ -1,40 +1,58 @@
 #!/bin/bash
 
-## Remove any pre installed docker packages : 
+install_ubuntu() {
+    ## Remove any pre installed docker packages : 
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+    cd /var/lib
+    rm -r docker
+    rm `which docker-compose`
+    rm `which docker-machine`
+    ## Install using the repository:
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    ## Add Dockers official GPG key & stable repo
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    ## Install Docker latest
+    sudo apt-get update ; clear
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo service docker start
+    ## curl -fsSL https://get.docker.com -o get-docker.sh
+    ## sudo sh get-docker.sh
+    echo "starting the installation of docker-compose ...."
+    echo " "
+    sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo " "
+    echo "Validating the versions Installed"
+    echo "`docker -v` - is installed Successfully"
+    echo "`docker-compose -v` - is installed Successfully"
+    exit 0
+}
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-cd /var/lib
-rm -r docker
-rm `which docker-compose`
-rm `which docker-machine`
+install_centos() {
 
-## Install using the repository:
+    sudo yum remove docker-client-latest docker-latest docker-latest-logrotate docker-logrotate docker-engine
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install docker-ce docker-ce-cli containerd.io
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+}
+################ MAIN ###################
 
-## Add Dockers official GPG key & stable repo
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-## Install Docker latest
-
-sudo apt-get update ; clear
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo service docker start
-
-## curl -fsSL https://get.docker.com -o get-docker.sh
-## sudo sh get-docker.sh
-
-echo "starting the installation of docker-compose ...."
-echo " "
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo " "
-echo "Validating the versions Installed"
-
-echo "`docker -v` - is installed Successfully"
-echo "`docker-compose -v` - is installed Successfully"
-
+if [ -f /etc/os-release ];then
+   osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
+   echo $osname
+   if [ $osname == "ubuntu" ];then
+       install_ubuntu
+   elif [ $osname == "amzn" ];then
+       install_centos
+   else [ $osname == "amzn" ];then
+       install_centos
+  fi
+else
+   echo "can not locate /etc/os-release - unable find the osname"
+   exit 8
+fi
 exit 0
+
---
+++ docker/install/installDocker.sh_bkp
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## Remove any pre installed docker packages : 
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+cd /var/lib
+rm -r docker
+rm `which docker-compose`
+rm `which docker-machine`
+
+## Install using the repository:
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+## Add Dockers official GPG key & stable repo
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+## Install Docker latest
+
+sudo apt-get update ; clear
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo service docker start
+
+## curl -fsSL https://get.docker.com -o get-docker.sh
+## sudo sh get-docker.sh
+
+echo "starting the installation of docker-compose ...."
+echo " "
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo " "
+echo "Validating the versions Installed"
+
+echo "`docker -v` - is installed Successfully"
+echo "`docker-compose -v` - is installed Successfully"
+
+exit 0

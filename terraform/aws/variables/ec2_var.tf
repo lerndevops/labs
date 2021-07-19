b16bd66dff7e5be4b3636e2c2692dde702f1aa97
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-19 13:46:27 +0530

    Update ec2_var.tf

--- terraform/aws/variables/ec2_var.tf
+++ terraform/aws/variables/ec2_var.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "test" {
   ami           = "ami-05692172625678b4e"
   instance_type = var.instancetype
 }
+
+#terraform plan will show the assignment
+#terraform plan -var="instancetype=t2.small"
+#If we dont define any default, if we dont give anything as commandline arg,  it explicitly asks to enter value.
+#terraform.tfvars ----> This file can be used to give values of variables. 

b5dba8caccca921de0647966ad7b3331114f0e30
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-19 13:54:43 +0530

    Update ec2_var.tf

--- terraform/aws/variables/ec2_var.tf
+++ terraform/aws/variables/ec2_var.tf
@@ -7,4 +7,5 @@ resource "aws_instance" "test" {
 #terraform plan -var="instancetype=t2.small"
 #If we dont define any default, if we dont give anything as commandline arg,  it explicitly asks to enter value.
 #terraform.tfvars ----> This file can be used to give values of variables. This is stanard name
-#If we want to use custom Name for tfvars say custom.tfvars, then we need to explicitly give it like  *terraform plan -var-file="custom.tfvars"*
+#If we want to use custom Name for tfvars say custom.tfvars, then we need to explicitly give it like  terraform plan -var-file="custom.tfvars"
+#IF we want to use Linux Environment variable, use "setx TF_VAR_instancetype t2.medium". 

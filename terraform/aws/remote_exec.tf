fd970744cddca3277135933dbdfa405a12d47bbe
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-10-06 19:51:21 +0530

    Update remote_exec.tf

--- terraform/aws/remote_exec.tf
+++ terraform/aws/remote_exec.tf
@@ -1,19 +1,20 @@
 provider "aws" {
 #  access_key = "accesskey_here"
 #  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
 resource "aws_instance" "test" {
   ami           = "ami-0b9064170e32bde34"
   instance_type = "t2.micro"
-  key_name = "edusep13"
+  key_name = "test"
+  security_groups = ["test"]
 
   connection {
       type     = "ssh"
       user     = "ubuntu"
       host     = self.public_ip
-      private_key = file("./edusep13.pem")
+      private_key = file("./test.pem")
   }
   provisioner "file" {
       source      = "/root/myterraproject/setup-user.sh"

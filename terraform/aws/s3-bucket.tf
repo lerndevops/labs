178cefb8834130b68b001b2506315547eda1c668
saiharshitach <96222425+saiharshitach@users.noreply.github.com>
2021-12-27 18:41:00 +0530

    Update s3-bucket.tf
    s3 working configs

--- terraform/aws/s3-bucket.tf
+++ terraform/aws/s3-bucket.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "s3b" {
-  bucket = "bucket1"
+resource "aws_s3_bucket" "testbucketforharshita" {
+  bucket = "backends3dec27"
   acl    = "private"
 
   tags = {
-    Name        = "bucket1"
+    Name        = "backends3dec27"
     Environment = "Dev"
   }
 }

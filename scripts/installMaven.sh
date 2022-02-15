67f8a7c9ce4c93fd5f3437d7c28f9ff8d6bdb8ba
lerndevops <lerndevops@gmail.com>
2022-02-15 19:57:26 +0530

    scripts

---
+++ scripts/installMaven.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install Maven
+MAVEN_VERSION=3.8.4
+sudo wget https://downloads.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -P /tmp
+sudo tar -xzf /tmp/apache-maven-$MAVEN_VERSION-bin.tar.gz -C  /opt/
+sudo mv /opt/apache-maven-$MAVEN_VERSION /opt/maven
+exit 0
---
+++ scripts/installTerraform.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -f /etc/os-release ];then
+   osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
+   echo $osname
+else
+   echo "can not locate /etc/os-release - unable find the osname"
+   exit 8
+fi
+
+if [ $osname == "ubuntu" ];then
+
+  clear
+  sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update
+  sudo apt-get install terraform
+  
+elif [ $osname == "amzn" ]; then
+
+  clear
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+  sudo yum update
+  sudo yum install -y terraform
+
+elif [ $osname == "centos" ];then
+
+  clear
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+  sudo yum update
+  sudo yum install -y terraform
+fi
+
+exit 0

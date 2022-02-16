a6cedd4662324e41bee1c8903a982b717e8cdbfd
lerndevops <45846027+lerndevops@users.noreply.github.com>
2022-02-16 07:50:25 +0530

    Update installK8S.sh

--- scripts/installK8S.sh
+++ scripts/installK8S.sh
@@ -68,15 +68,15 @@ install_centos() {
 
     # Install kubeadm, kubelet, kubectl 
 
-    cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-    exclude=kubelet kubeadm kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
 EOF
   
     # Set SELinux in permissive mode (effectively disabling it)

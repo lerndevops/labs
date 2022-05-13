2448c92531e8ce9e59b4f217342c5f11a4cc4805
nareshwart <lerndevops@gmail.com>
2022-05-13 14:10:17 +0530

    docker

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -53,7 +53,15 @@ install_centos() {
     sudo yum install -y yum-utils   ## device-mapper-persistent-data lvm2
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io
-
+    if [ $? -eq 0 ];then
+      echo "docker-ce is successfully installed"
+      sudo apt-mark hold docker-ce
+      sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
+      sudo service docker restart ; clear
+    else
+      echo "issue with docker-ce installation - process abort"
+      exit 1
+    fi
 }
 ################ MAIN ###################
 

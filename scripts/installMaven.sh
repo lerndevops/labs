19aa9aab3a494817a6198ad84535b597282fe2d2
leaddevops <36464863+leaddevops@users.noreply.github.com>
2022-06-15 22:39:35 +0530

    Update installMaven.sh

--- scripts/installMaven.sh
+++ scripts/installMaven.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install Maven
-MAVEN_VERSION=3.8.5
+MAVEN_VERSION=3.8.6
 sudo wget https://downloads.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -P /tmp
 sudo tar -xzf /tmp/apache-maven-$MAVEN_VERSION-bin.tar.gz -C  /opt/
 sudo mv /opt/apache-maven-$MAVEN_VERSION /opt/maven

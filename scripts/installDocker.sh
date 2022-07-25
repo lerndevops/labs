d0591e68378c7959004bf225af3512ce7cb42918
leaddevops <36464863+leaddevops@users.noreply.github.com>
2022-07-25 15:31:21 +0530

    Update installDocker.sh

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -20,7 +20,7 @@ install_ubuntu() {
     ## Add Dockers official GPG key & stable repo
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
     #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     ## Install Docker latest

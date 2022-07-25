90144fd80d7af50af1fa6746f9c32a055d7c5cc8
leaddevops <36464863+leaddevops@users.noreply.github.com>
2022-07-25 23:45:38 +0530

    Update installDocker.sh

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -4,7 +4,7 @@ install_ubuntu() {
     ## Remove any pre installed docker packages:
     which docker
     if [ $? -eq 0 ];then
-       sudo systemctl stop docker 
+       sudo systemctl stop docker
        sudo apt-get remove -y docker-ce docker-engine docker.io containerd runc
        sudo apt-get purge -y docker-ce docker-ce-cli containerd.io
        sudo apt -y autoremove
@@ -20,7 +20,7 @@ install_ubuntu() {
     ## Add Dockers official GPG key & stable repo
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
     #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     ## Install Docker latest
@@ -30,7 +30,7 @@ install_ubuntu() {
     if [ $? -eq 0 ];then
        if [ -f /etc/docker/daemon.json ];then
          echo "cgroup config is already configured skipping.."
-       else 
+       else
           echo "docker-ce is successfully installed"
           yum install -y wget
           sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
@@ -50,7 +50,7 @@ install_centos() {
        sudo yum remove docker-client-latest docker-latest docker-latest-logrotate docker-logrotate docker-engine
        cd /var/lib
        rm -rf docker
-       yum clean 
+       yum clean
     else
        echo "docker is not installed... continue to install"
     fi
@@ -61,7 +61,7 @@ install_centos() {
     if [ $? -eq 0 ];then
        if [ -f /etc/docker/daemon.json ];then
          echo "cgroup config is already configured skipping.."
-       else 
+       else
           echo "docker-ce is successfully installed"
           yum install -y wget
           sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker

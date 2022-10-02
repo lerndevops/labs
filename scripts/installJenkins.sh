b44dc5b69cc7d1bd8b3b95bb6cc55c1709267797
leaddevops <36464863+leaddevops@users.noreply.github.com>
2022-10-02 22:07:56 +0530

    Update installJenkins.sh

--- scripts/installJenkins.sh
+++ scripts/installJenkins.sh
@@ -2,17 +2,17 @@
 
 install_ubuntu() {
 
-    #Install JAVA8
+    #Install JAVA11
     sudo add-apt-repository ppa:openjdk-r/ppa
     sudo apt-get update
-    sudo apt-get install -y openjdk-8-jdk
+    sudo apt-get install -y openjdk-11-jdk
 
     #Install Jenkins
 
-    wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-    echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+    sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+    sudo echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
     sudo apt-get update
-    sudo apt-get install -y jenkins
+    sudo apt-get install jenkins
     sudo systemctl start jenkins
 }
 

d246aa53ef512e148c83a361d35cd85ce0e38aef
leaddevops <leaddevops@gmail.com>
2022-12-29 14:04:40 +0530

    ansible

--- ansible/IaC/list-ec2-instances.sh
+++ ansible/IaC/list-ec2-instances.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --filters Name=instance-state-name,Values=running --output text
-aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value}" --filters Name=instance-state-name,Values=running --output table
+aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value}" --filters Name=instance-state-name,Values=running --output text

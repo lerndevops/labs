80c4e3dc8a1e31b8cf78327a740f24988e1de101
leaddevops <36464863+leaddevops@users.noreply.github.com>
2023-07-26 10:53:49 +0530

    update setupk8s-single-node-cluster.sh

--- scripts/setupk8s-single-node-cluster.sh
+++ scripts/setupk8s-single-node-cluster.sh
@@ -6,26 +6,34 @@ prepare_node() {
     which docker
     if [ $? -eq 0 ];then
        echo "docker is already installed no action required.."
-       sudo systemctl restart docker
+       sudo systemctl restart docker.service
     else
        echo "docker is not installed.. continue to install"
        wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
-       bash /tmp/installDocker.sh
+       sudo bash /tmp/installDocker.sh
     fi 
+    which cri-dockerd
+    if [ $? -eq 0 ];then
+       echo "docker is already installed no action required.."
+       sudo systemctl restart cri-docker.service
+    else
+       echo "docker is not installed.. continue to install"
+       wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
+       sudo bash /tmp/installCRIDockerd.sh
+    fi
     which kubelet
     if [ $? -eq 0 ];then
        echo "kubernetes modules are already installed no action required.."
     else
        echo "kuberenetes modules are not installed.. continue to install"
-       sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S-v1-23.sh -P /tmp
-       sudo chmod 755 /tmp/installK8S-v1-23.sh
-       sudo bash /tmp/installK8S-v1-23.sh
+       sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S.sh -P /tmp
+       sudo bash /tmp/installK8S.sh
     fi
   
 }
 create_cluster() {
 
-    kubeadm init --ignore-preflight-errors=all
+    kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=all
     if [ $? -eq 0 ];then
        echo "kuberenetes master initilized successfully"
 
@@ -35,9 +43,9 @@ create_cluster() {
 
        if [ -f $HOME/.kube/config ];then
 
-            sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-            mnodename=`kubectl get nodes | grep master | awk '{print $1}'`
-            kubectl taint node $mnodename node-role.kubernetes.io/master:NoSchedule-
+            sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
+            mnodename=`kubectl get nodes | grep control-plane | awk '{print $1}'`
+            kubectl taint node $mnodename node-role.kubernetes.io/control-plane:NoSchedule-
 
        else
           echo "config file missing under user home.."

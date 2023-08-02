377f06a569b7cd1f4b041d71278028fb1044747b
leaddevops <36464863+leaddevops@users.noreply.github.com>
2023-08-02 13:34:57 +0530

    update genSelfSingedCACerts.sh

--- scripts/genSelfSingedCACerts.sh
+++ scripts/genSelfSingedCACerts.sh
@@ -10,6 +10,8 @@ if [ $? -eq 0 ]; then
   openssl genrsa -out $HOME/ssl/server.key 2048
   openssl req -new -key $HOME/ssl/server.key -out $HOME/ssl/server.csr -subj "/CN=server/O=devops"
   openssl x509 -req -in $HOME/ssl/server.csr -CA $HOME/ssl/ca.crt -CAkey $HOME/ssl/ca.key -CAcreateserial -out $HOME/ssl/server.crt -days 1000
+  echo "certs are generated under $HOME/ssl folder & are ready to use now.. "
+  ls -l $HOME/ssl
 else
   echo "openssl is missing please install.." 
 fi
